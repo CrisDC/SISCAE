3ee7e22e2c34cba0128ba9e980fbd5df75f8a4d9
CrisDC <cristhianw11@hotmail.com>
2018-08-01 14:29:38 -0500

    Mapper de Jairo implementados

--- siscae/src/test/java/pe/edu/unmsm/fisi/siscae/service/AlumnoServiceTest.java
+++ siscae/src/test/java/pe/edu/unmsm/fisi/siscae/service/AlumnoServiceTest.java
@@ -41,7 +41,7 @@ public class AlumnoServiceTest {
 
 		alumnoTest.setEstadoAlumno("INACTIVO");
 		alumnoService.actualizarAlumno(alumnoTest);
-
+		
 		alumnoService.eliminarAlumno(alumnoTest);
 	}
 
