db843d2b1d12749af7b498caa928fc82bbcafdd1
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2018-09-26 11:04:58 -0500

    Peticion Ajax de prestamo casi completa

--- siscae/src/main/webapp/resources/js/pagina/consulta/movimientos/prestamos/prestamos.js
+++ siscae/src/main/webapp/resources/js/pagina/consulta/movimientos/prestamos/prestamos.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
       $('body #for-each').on('click', 'button', function(){
         let idRecurso = $(this).attr('key');
         let numRecurso = $(this).attr('id');
-        
         swal({
 			title: "Solicitud de recurso "+numRecurso,
 			text: "Ingrese su carnet de biblioteca por el scanner",
@@ -23,36 +22,34 @@ $(document).ready(function(){
 			icon: "resources/images/nuevo_gif.gif",
 		
 		}).then(function (inputValue) {
+			
+			var prestamo ={
+		        	"idRecurso": idRecurso,
+		        	"numDocumento": inputValue
+		    };
+			prestamo.
 			$.ajax({
-                // la URL para la petición
-                url : 'movimientoPrestamo',
-                // la información a enviar
-                // (también es posible utilizar una cadena de datos)
-                data : 'idRecurso='+idRecurso+'&numDocumento='+inputValue,
-                // especifica si será una petición POST o GET
+                url : "movimientoPrestamo",
                 type : 'POST',
-             
-                // código a ejecutar si la petición es satisfactoria;
-                // la respuesta es pasada como argumento a la función
-                success : function(json) {
-                    $('<h1/>').text(json.title).appendTo('body');
-                    $('<div class="content"/>')
-                        .html(json.html).appendTo('body');
-                    swal('Peticion de recurso con exito');
-                },
-             
-                // código a ejecutar si la petición falla;
-                // son pasados como argumentos a la función
-                // el objeto de la petición en crudo y código de estatus de la petición
-                error : function(xhr, status) {
-                    swal('Problemas con el servidor');
-                },
-                /*
-                // código a ejecutar sin importar si la petición falló o no
-                complete : function(xhr, status) {
-                    alert('Petición realizada');
-                }
-                });*/
+                data : JSON.stringify(prestamo),
+    			beforeSend : function(xhr) {
+    				xhr.setRequestHeader('Content-Type', 'application/json');
+    				//xhr.setRequestHeader("X-CSRF-TOKEN", token);
+    			},
+    			statusCode : {
+    				400 : function(response) {
+    					swal(response.responseJSON);
+    				}
+    			},
+    			success : function(response) {
+    				swal('Peticion realizada con exito');
+    			},
+    			error : function(response) {
+    				swal('Problemas con el servidor');
+    			},
+    			complete : function(response) {
+    				
+    			}
 
 			}, function (dismiss) {
 			  // dismiss can be 'cancel', 'overlay',
