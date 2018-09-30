e131b0d2ac2789996a44904b0678a3c60619a110
CrisDC <cristhianw11@hotmail.com>
2018-09-29 19:08:18 -0500

    Commit

--- siscae/src/main/webapp/resources/js/pagina/mantenimiento/areaAdministrativo/areaAdministrativo.js
+++ siscae/src/main/webapp/resources/js/pagina/mantenimiento/areaAdministrativo/areaAdministrativo.js
@@ -149,7 +149,10 @@ $(document).ready(function() {
 				$local.$registrarMantenimiento.attr("disabled", false).find("i").addClass("fa-floppy-o").removeClass("fa-spinner fa-pulse fa-fw");
 			}
 		});
-		$local.$tablaMantenimiento.children("tbody").on("click", ".actualizar", function() {
+		
+	});
+	
+	$local.$tablaMantenimiento.children("tbody").on("click", ".actualizar", function() {
 		$funcionUtil.prepararFormularioActualizacion($formMantenimiento);
 		$local.$filaSeleccionada = $(this).parents("tr");
 		var areaAdministrativo = $local.tablaMantenimiento.row($local.$filaSeleccionada).data();
@@ -159,7 +162,7 @@ $(document).ready(function() {
 		$local.$registrarMantenimiento.addClass("hidden");
 		$local.$modalMantenimiento.PopupWindow("open");
 	});
-	});
+	
 	$local.$actualizarMantenimiento.on("click", function() {
 		if (!$formMantenimiento.valid()) {
 			return;
