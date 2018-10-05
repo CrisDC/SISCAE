acaca2be82d1e340b42a8c19d57107ae9e9ac487
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2018-10-05 12:42:19 -0500

    Cambiando html

--- siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/consulta/ConsultaController.java
+++ siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/consulta/ConsultaController.java
@@ -1,5 +1,7 @@
 package pe.edu.unmsm.fisi.siscae.controller.consulta;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import antlr.collections.List;
 import pe.edu.unmsm.fisi.siscae.aspecto.anotacion.Audit;
 import pe.edu.unmsm.fisi.siscae.aspecto.enumeracion.Accion;
 import pe.edu.unmsm.fisi.siscae.aspecto.enumeracion.Comentario;
@@ -14,6 +17,11 @@ import pe.edu.unmsm.fisi.siscae.aspecto.enumeracion.Tipo;
 import pe.edu.unmsm.fisi.siscae.configuracion.security.SecurityContextFacade;
 import pe.edu.unmsm.fisi.siscae.controller.excepcion.anotacion.Vista;
 import pe.edu.unmsm.fisi.siscae.model.criterio.ConsultaPrestamosCriterioBusqueda;
+import pe.edu.unmsm.fisi.siscae.model.mantenimiento.Administrativo;
+import pe.edu.unmsm.fisi.siscae.model.mantenimiento.AreaAdministrativo;
+import pe.edu.unmsm.fisi.siscae.model.mantenimiento.Usuario;
+import pe.edu.unmsm.fisi.siscae.service.IAdministrativoService;
+import pe.edu.unmsm.fisi.siscae.service.IAreaAdministrativoService;
 import pe.edu.unmsm.fisi.siscae.service.IConsultaPrestamosService;
 import pe.edu.unmsm.fisi.siscae.service.IUsuarioService;
 
@@ -28,22 +36,36 @@ public @Controller class ConsultaController
 	private static final String CONSULTA_NUEVOS_SOLICITANTES = CONSULTA_MOVIMIENTOS + "nuevosSolicitantes";
 	
 	private @Autowired IConsultaPrestamosService consultaPrestamosService;
-	private @Autowired IUsuarioService iUsuarioService;
-
+	private @Autowired IUsuarioService usuarioService;
+	private @Autowired IAreaAdministrativoService areaAdministrativoService;
 	
     @Audit(tipo = Tipo.CON_MOV_ESTADO_AREA)
     @GetMapping("/{consulta:estadoArea}")
     public String irPaginaConsultaPrestamosEstadoArea(@PathVariable String consulta, ModelMap model)
     {
+    	//Capturamos el idPersona del usuario que inicio sesion
+    	System.out.println(SecurityContextFacade.getAuthenticatedUser());
+    	int idAdministrativo=3; // idPersona == idAdministrativo, Aqui debemos asignar lo del usuario al
+    	
+    	
+    	//Provisional, hasta preguntar andres si se puede hacer una consulta por un criterio (buscar AreaAdministrativo por idAdministrativo)
+    	AreaAdministrativo areaAdministrativo = null;
+    	
+    	ArrayList<AreaAdministrativo> listaAreaAdministrativo = (ArrayList) areaAdministrativoService.buscarTodos();  
+    	for(int i=0;i<listaAreaAdministrativo.size();i++){
+    		if(listaAreaAdministrativo.get(i).getIdAdministrativo()==idAdministrativo){
+    			areaAdministrativo = listaAreaAdministrativo.get(i);
+    		}
+    	}
+    	
+    	
     	ConsultaPrestamosCriterioBusqueda criterioBusqueda = new ConsultaPrestamosCriterioBusqueda();
-    	criterioBusqueda.setAreaEstudio("BIBLIOTECA");
-    	
-    	
-    	System.out.println(SecurityContextFacade.obtenerNombreUsuario());
+    	criterioBusqueda.setAreaEstudio(areaAdministrativo.getNombreAreaEstudio());
     	
     	
     	model.addAttribute("prestamos",  consultaPrestamosService.buscarPorCriterio(criterioBusqueda));
     	model.addAttribute("consulta", consulta);
+    	model.addAttribute("areaAdministrativo", areaAdministrativo);
         return CONSULTA_ESTADO_AREA;
     }
     
