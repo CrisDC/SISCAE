eba0fc0ebfc41cea2b306f9a97ba338d1093c9f2
Vieri Garcia <vieri.garcia@unmsm.edu.pe>
2018-10-06 13:27:30 -0500

    REST CONTROLLER REPORTE ESTADISTICA

---
+++ siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/reporte/rest/reporteEstadisticoPrestamoContoller.java
@@ -0,0 +1,26 @@
+package pe.edu.unmsm.fisi.siscae.controller.reporte.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import pe.edu.unmsm.fisi.siscae.model.criterio.ReporteEstadisticaPrestamosCriterioBusqueda;
+import pe.edu.unmsm.fisi.siscae.model.reporte.ReporteEstadisticoPrestamos;
+import pe.edu.unmsm.fisi.siscae.service.IReporteEstadisticaPrestamosService;
+
+@RequestMapping("/reporteEstadisticoPrestamo")
+public @RestController class reporteEstadisticoPrestamoContoller {
+	private @Autowired IReporteEstadisticaPrestamosService reporteEstadisticaPrestamosService;
+	@GetMapping(params = "accion=buscarTodos")
+	public List<ReporteEstadisticoPrestamos> buscarTodos() {
+		return reporteEstadisticaPrestamosService.buscarTodos();
+	}
+	@GetMapping(params = "accion=buscarPorCriterio")
+	public List<ReporteEstadisticoPrestamos> buscarPorCriterio(ReporteEstadisticaPrestamosCriterioBusqueda criterioBusqueda) {
+		return reporteEstadisticaPrestamosService.buscarPorCriterio(criterioBusqueda);
+	}
+}
