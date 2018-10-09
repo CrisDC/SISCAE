400aced7d1df06ef37c955dec66563e839407819
Mario Cortez Escarate <mario.cortez1@unmsm.edu.pe>
2018-10-08 21:56:33 -0500

    EFECTO COMBO JS SOLICITANTES

--- siscae/src/main/webapp/resources/js/pagina/consulta/movimientos/solicitantes/solicitantes.js
+++ siscae/src/main/webapp/resources/js/pagina/consulta/movimientos/solicitantes/solicitantes.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
+    
+    $('#fformulario-tipo-academico').css('display', 'none');
+
+    $('#formulario-codigo').css('display', 'none');
+
+    $('#formulario-escuela').css('display', 'none');
+});
+
+
+$(document).on('change', '#formulario-ocupacion', function(event) {
+    let valor = $("#formulario-ocupacion option:selected").text();
+    if(valor=='ALUMNO'){
+    		$('#fformulario-tipo-academico').css('display', 'block');
+
+    	    $('#formulario-codigo').css('display', 'block');
+
+    	    $('#formulario-escuela').css('display', 'block');
+    }else{
+        $("#codigo").css("display", "none");
+    }
 });
