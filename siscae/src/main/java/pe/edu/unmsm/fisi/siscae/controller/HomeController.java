46eff64ea174503a07e72c9f62e6fa6feea3ff4b
jbernilla <juan.bernilla1@unmsm.edu.pe>
2019-08-25 14:06:49 -0500

    changes seguridad 02

--- siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/HomeController.java
+++ siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/HomeController.java
@@ -6,14 +6,23 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import pe.edu.unmsm.fisi.siscae.configuracion.security.SecurityContextFacade;
 import pe.edu.unmsm.fisi.siscae.controller.excepcion.anotacion.Vista;
 import pe.edu.unmsm.fisi.siscae.model.consulta.ConsultaAdministrativo;
 import pe.edu.unmsm.fisi.siscae.model.criterio.ConsultaAdministrativoCriterioBusqueda;
+import pe.edu.unmsm.fisi.siscae.model.seguridad.Cambiocontra;
 import pe.edu.unmsm.fisi.siscae.service.IConsultaAdministrativoService;
 import pe.edu.unmsm.fisi.siscae.service.IParametroGeneralService;
 
@@ -48,6 +57,57 @@ public @Controller class HomeController
 		model.addAttribute("usuario",principal.getName());
 		return "redirect:/movimiento/estadoArea";
 	}
+    @CrossOrigin(origins = "http://localhost:8280")
+    @GetMapping("/hola")
+	public String irConfiguracionContrasenia(Model model, Principal principal) {
+    	BCryptPasswordEncoder encriptador = new BCryptPasswordEncoder();
+    	System.out.println("------------------------------------------------------");
+    	System.out.println(encriptador.encode("Bica"));
+    	System.out.println(encriptador.encode("12345"));
+    	if(encriptador.matches("Bica", encriptador.encode("Bica"))) {
+    		System.out.println("si es igual");
+    	}else {
+    		System.out.println("no es igual");
+    	}
+    	if(encriptador.matches("Bica2", encriptador.encode("Bica"))) {
+    		System.out.println("si es igual");
+    	}else {
+    		System.out.println("no es igual");
+    	}
+		model.addAttribute("usuario",principal.getName());
+		return "seguras/inicio";
+	}
+    
+    
+    //@RequestMapping(value = "/username", method = RequestMethod.GET)
+    @GetMapping("/username")
+    @ResponseBody
+    public String name() {
+    	String currentUserName= "";
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	if (!(authentication instanceof AnonymousAuthenticationToken)) {
+    	    currentUserName = authentication.getName(); 
+    	}
+    	System.out.println("**************************************");
+    	System.out.println("nombre user es "+ currentUserName );
+    	return currentUserName;
+    }
+    
+    @PostMapping("/cambiocontra")
+    @ResponseBody	
+    public Cambiocontra cambiar(@RequestBody Cambiocontra c) {
+    	Cambiocontra nuevo = new Cambiocontra();
+    	BCryptPasswordEncoder encriptador = new BCryptPasswordEncoder();
+    	if(encriptador.matches(c.getIngrepass(), c.getPass())) {
+    		nuevo.setNuevopass(encriptador.encode(c.getNuevopass()));
+    	}else {
+    		nuevo.setNuevopass("incorrecto");
+    	}
+    	nuevo.setPass(c.getPass());
+    	nuevo.setUsername(c.getUsername());
+		nuevo.setIngrepass(c.getIngrepass());
+    	return nuevo;
+    }
     
    
 }
