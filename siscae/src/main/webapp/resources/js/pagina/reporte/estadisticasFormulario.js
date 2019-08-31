94a6dde829f59aa6abaaf31b91c17507dc3324a0
Erik Garcia Tacas <erik.fisi16@gmail.com>
2019-08-30 19:57:04 -0500

    formulario MasFrecuentes validación

--- siscae/src/main/webapp/resources/js/pagina/reporte/estadisticasFormulario.js
+++ siscae/src/main/webapp/resources/js/pagina/reporte/estadisticasFormulario.js
@@ -98,4 +98,37 @@ $(document).ready(function() {
 			}
 		}
 	});
+	
+	$formMasFrecuentes.validate({
+		rules : {
+			areasEstudio: {
+				required : true
+			},
+						
+		},
+		messages : {
+			areasEstudio : {
+				required: "Elige un area de Estudio"
+			}
+		},
+		highlight : function(element) {
+			$(element).parents(".group, .form-group").first().addClass('has-error');
+		},
+		unhighlight : function(element) {
+			$(element).parents(".group, .form-group").first().removeClass('has-error');
+		},
+		errorElement : 'span',
+		errorClass : 'help-block',
+		errorPlacement : function(error, element) {
+			if (element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else if (element.parent().find("span.select2").length > 0) {
+				error.appendTo(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+
+	
+	  });
 });
