70c396753bdb67d776dfbfd49b8745b9f03d1b93
Angel <angelcayatopa98@gnail.com>
2019-11-23 21:37:31 -0500

    cambio administrativo con permisos primera fase

--- siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/mantenimiento/rest/AdministrativoController.java
+++ siscae/src/main/java/pe/edu/unmsm/fisi/siscae/controller/mantenimiento/rest/AdministrativoController.java
@@ -6,6 +6,7 @@ import javax.validation.groups.Default;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,6 +35,7 @@ public @RestController class AdministrativoController {
 
 	private @Autowired IAdministrativoService administrativoService;
 	@Audit(accion = Accion.CONSULTA, comentario = Comentario.ConsultaTodos)
+	@PreAuthorize("hasPermission('MANT_CITAS', '[2],[3],[4]')")
 	@GetMapping(params = "accion=buscarTodos")
 	public List<Administrativo> buscarTodos(){
 		
@@ -41,6 +43,7 @@ public @RestController class AdministrativoController {
 		
 	}
 	@Audit(accion = Accion.REGISTRO, comentario = Comentario.Registro)
+	@PreAuthorize("hasPermission('MANT_ADMINISTRATIVO', '1')")
 	@PostMapping
 	public ResponseEntity<?> registrarAdministrativo(
 		@Validated({ Default.class, IRegistro.class }) @RequestBody Administrativo administrativo,
